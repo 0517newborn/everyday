1d94d26ecb96f5552fbe774fff950c159f0af820
mtkn <mtkn.misty@gmail.com>
2013-02-19 23:00:42 +0900

    バグ修正、コーディングスタイルの統一

--- Chapter6/mails_by_day_of_month_orig.r
+++ Chapter6/mails_by_day_of_month_orig.r
@@ -13,10 +13,10 @@ sent_count <- data.frame(table(elements))$Freq
 
 df <- data.frame(days=1:31,inbox=inbox_count,sent=sent_count)
 
-ggplot(data=df) + scale_shape_manual(name="Mailbox", values=c(2,3)) +
+ggplot(data=df) + scale_shape_manual(name="Mailbox", value=c(2,3)) +
   geom_point(aes(x=days,y=inbox, shape='inbox')) +
-  geom_smooth(aes(x=days,y=inbox, shape='inbox')) +
+  geom_smooth(aes(x=days,y=inbox, group=1)) +
   geom_point(aes(x=days,y=sent, shape='sent')) +
-  geom_smooth(aes(x=days,y=sent, shape='sent')) +
+  geom_smooth(aes(x=days,y=sent, group=2)) +
   scale_y_continuous('number of emails') +
-  scale_x_continuous('day of month')
+  scale_x_discrete('day of month')
